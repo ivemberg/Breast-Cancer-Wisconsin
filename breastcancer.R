1e5de8b8186cefe652651faebbcddc2cba283af3
Umberto <okamirvs@gmail.com>
2020-03-05 03:38:54 +0100

    final project

--- breastcancer.R
+++ breastcancer.R
@@ -12,7 +12,7 @@
 # Set workspace path
 # setwd("~/git/breastcancer")
 
-#Install packages and library
+# Install packages and library
 install.packages(c("caret","FactoMineR","factoextra","corrplot","pROC","funModeling", "gridExtra"))
 library(caret)
 library(FactoMineR) 
@@ -22,14 +22,15 @@ library(pROC)
 library(funModeling)
 library(gridExtra)
 
-#Dataset
-dataset = read.csv("data.csv",
+# Dataset
+dataset <- read.csv("data.csv",
                     header = TRUE,
                     na.strings = "?",
                     colClasses=c("diagnosis"="factor"),
                     stringsAsFactors=F)
 # Remove ID
-dataset = dataset[,-1]
+dataset <- dataset[,-1]
+
 # Control presence of null value
 na_count <- sapply(dataset, function(y) sum(length(which(is.na(y)))))
 na_count <- data.frame(na_count)
@@ -130,7 +131,7 @@ ggplot(trainset, aes(x=trainset$diagnosis, fill=trainset$diagnosis)) +
 
 
 # Correlation
-corr = cor(trainset[,2:31])
+corr <- cor(trainset[,2:31])
 corrplot(corr,type="lower",tl.col=1,tl.cex=0.7)
 
 # Example of correlated variables (high redundancy)
@@ -153,14 +154,14 @@ ggplot(trainset, aes(x = trainset$fractal_dimension_se, y = trainset$area_worst,
 pca <- PCA(trainset[,2:31], scale.unit = TRUE, ncp = 10, graph = TRUE) 
 
 # Examinate the PCA's result with the eigenvalues
-eig.val = get_eigenvalue(pca)
+eig.val <- get_eigenvalue(pca)
 eig.val
 
 # Graphical representation of eigenvalues
 fviz_eig(pca, addlabels = TRUE, ylim = c(0,45), title = "Eigenvalues")
 
 # Variables extraction
-var = get_pca_var(pca)
+var <- get_pca_var(pca)
 head(var$coord, 30)
 
 # Graphical representation of PCA's variables
@@ -181,41 +182,42 @@ p10 <- fviz_contrib(pca, choice="var", axes=10, fill="#6BB7C6", color="grey", to
 grid.arrange(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,ncol=5)
 
 # Model applicated only on PCA dimensions
-trg = predict(pca, trainset)
-trg = data.frame(trg$coord, trainset[1])
-tst = predict(pca, testset)
-tst = data.frame(tst$coord, testset[1])
+trg <- predict(pca, trainset)
+trg <- data.frame(trg$coord, trainset[1])
+tst <- predict(pca, testset)
+tst <- data.frame(tst$coord, testset[1])
 
 # 10-fold cross validation with 3 repeats
-control = trainControl(method="repeatedcv", number=10, repeats = 3)
-metric = "Accuracy"
+control <- trainControl(method="repeatedcv", number=10, repeats = 3)
+metric <- "Accuracy"
 
 # SVM
 set.seed(7)
 fit.svm <- train(diagnosis~., data=trg, method="svmRadial", metric=metric, trControl=control)
+
 # Neural networks
 set.seed(7)
 fit.nnet <- train(diagnosis~., data=trg, method="nnet", metric=metric, trControl=control, trace=FALSE)
 
-# Accuracy
-list_models = list(svm=fit.svm, nnet=fit.nnet)
-results = resamples(list_models)
+# Accuracy from 10-fold cross validation
+list_models <- list(svm=fit.svm, nnet=fit.nnet)
+results <- resamples(list_models)
 summary(results)
 
-accuracies <- data.frame(fit.svm$results$Accuracy, fit.nnet$results$Accuracy)
+accuracies <- data.frame(fit.svm$resample$Accuracy, fit.nnet$resample$Accuracy)
 names(accuracies) <- c(paste("SVM accuracy"), paste("NNET accuracy"))
 boxplot(accuracies,col="#BBE1FA",ylab="value" )
 
 # Predictions SVM (accuracy of the model on our validation set)
-predictions_svm = predict(fit.svm, tst)
-cm_svm = confusionMatrix(predictions_svm, tst$diagnosis)
+predictions_svm <- predict(fit.svm, tst)
+cm_svm <- confusionMatrix(predictions_svm, tst$diagnosis)
 cm_svm
 fourfoldplot(cm_svm$table, color = c("#CE1824","#60B267"),
              conf.level = 0, margin = 1, main = "Confusion Matrix SVM")
 
 # Predictions NNET (accuracy of the model on our validation set)
-predictions_nnet = predict(fit.nnet, tst)
-cm_nnet = confusionMatrix(predictions_nnet, tst$diagnosis)
+predictions_nnet <- predict(fit.nnet, tst)
+cm_nnet <- confusionMatrix(predictions_nnet, tst$diagnosis)
 cm_nnet
 fourfoldplot(cm_nnet$table, color = c("#CE1824","#60B267"),
              conf.level = 0, margin = 1, main = "Confusion Matrix NNET")
@@ -224,46 +226,47 @@ fourfoldplot(cm_nnet$table, color = c("#CE1824","#60B267"),
 # Model applicated on all attributes
 
 # 10-fold cross validation with 3 repeats
-control = trainControl(method="repeatedcv", number=10, repeats = 3)
-metric = "Accuracy"
+control <- trainControl(method="repeatedcv", number=10, repeats = 3)
+metric <- "Accuracy"
 
 # SVM
 set.seed(7)
 fit.svm <- train(diagnosis~., data=trainset, method="svmRadial", metric=metric, trControl=control)
+
 # Neural networks
 set.seed(7)
 fit.nnet <- train(diagnosis~., data=trainset, method="nnet", metric=metric, trControl=control, trace=FALSE)
 
-# Accuracy
-list_models = list(svm=fit.svm, nnet=fit.nnet)
-results = resamples(list_models)
+# Accuracy 10-fold cross validation
+list_models <- list(svm=fit.svm, nnet=fit.nnet)
+results <- resamples(list_models)
 summary(results)
 
-accuracies <- data.frame(fit.svm$results$Accuracy, fit.nnet$results$Accuracy)
+accuracies <- data.frame(fit.svm$resample$Accuracy, fit.nnet$resample$Accuracy)
 names(accuracies) <- c(paste("SVM accuracy"), paste("NNET accuracy"))
 boxplot(accuracies,col="#BBE1FA",ylab="value" )
 
 # Predictions SVM (accuracy of the model on our validation set)
-predictions_svm = predict(fit.svm, testset)
-cm_svm = confusionMatrix(predictions_svm, testset$diagnosis)
+predictions_svm <- predict(fit.svm, testset)
+cm_svm <- confusionMatrix(predictions_svm, testset$diagnosis)
 cm_svm
 fourfoldplot(cm_svm$table, color = c("#CE1824","#60B267"),
              conf.level = 0, margin = 1, main = "Confusion Matrix SVM")
 
 # Predictions NNET (accuracy of the model on our validation set)
-predictions_nnet = predict(fit.nnet, testset)
-cm_nnet = confusionMatrix(predictions_nnet, testset$diagnosis)
+predictions_nnet <- predict(fit.nnet, testset)
+cm_nnet <- confusionMatrix(predictions_nnet, testset$diagnosis)
 cm_nnet
 fourfoldplot(cm_nnet$table, color = c("#CE1824","#60B267"),
              conf.level = 0, margin = 1, main = "Confusion Matrix NNET")
 
 # Take the model with the best accurancy
-maxAcc = 0
+maxAcc <- 0
 for(item in list_models){
-  meanAcc = mean(item[["resample"]][["Accuracy"]])
+  meanAcc <- mean(item[["resample"]][["Accuracy"]])
   if(meanAcc>maxAcc){
-    maxAcc=meanAcc
-    finalModel=item
+    maxAcc <- meanAcc
+    finalModel<- item
   }
 }
 
@@ -280,14 +283,14 @@ bestModel <- readRDS("./finalModel.rds")
 print(bestModel)
 
 # Predictions (accuracy of the model on our validation set)
-predictions = predict(bestModel, testset)
+predictions <- predict(bestModel, testset)
 confusionMatrix(predictions, testset$diagnosis)
 
 # ROC
 
-#Set up the training control
-control = trainControl(method="repeatedcv", number=10, repeats=3, classProbs = TRUE, summaryFunction = twoClassSummary)
-metric = "ROC"
+# Set up the training control
+control <- trainControl(method="repeatedcv", number=10, repeats=3, classProbs = TRUE, summaryFunction = twoClassSummary)
+metric <- "ROC"
 
 # SVM
 set.seed(7)
@@ -298,22 +301,22 @@ set.seed(7)
 fit.nnet <- train(diagnosis~., data=trainset, method="nnet", metric=metric, trControl=control, trace=FALSE)
 
 # Make Predictions
-svm.probs = predict(fit.svm, testset, type = "prob")
-nnet.probs = predict(fit.nnet, testset, type = "prob")
+svm.probs <- predict(fit.svm, testset, type = "prob")
+nnet.probs <- predict(fit.nnet, testset, type = "prob")
 
 # Generate the ROC curve of each model, and plot the curve on the same figure
-svm.ROC = roc(testset$diagnosis, svm.probs$B, levels=levels(testset$diagnosis), direction = ">")
+svm.ROC <- roc(testset$diagnosis, svm.probs$B, levels=levels(testset$diagnosis), direction = ">")
 plot(svm.ROC, print.thres="best", col="orange")
 
-nnet.ROC = roc(testset$diagnosis, nnet.probs$B, levels=levels(testset$diagnosis), direction = ">")
+nnet.ROC <- roc(testset$diagnosis, nnet.probs$B, levels=levels(testset$diagnosis), direction = ">")
 plot(nnet.ROC, print.thres="best", col="red")
 
-# comparison of the AUC
+# Comparison of the AUC
 svm.ROC
 nnet.ROC 
 
-# comparison of the statistics of the generated performance measure
-cv.values = resamples(list(svm=fit.svm, nnet = fit.nnet)) 
+# Comparison of the statistics of the generated performance measure
+cv.values <- resamples(list(svm=fit.svm, nnet = fit.nnet)) 
 summary(cv.values) 
 
 # Plots
@@ -323,20 +326,20 @@ boxplot(ROCs,col="#BBE1FA",ylab="value", ylim=c(0.975,1))
 
 splom(cv.values, metric="ROC") 
 
-# timings required for training the models
+# Timings required for training the models
 cv.values$timings
 
 # Precision, Recall an F1 for SVM model
-realvalues = testset[,1]
+realvalues <- testset[,1]
 classLabelSVM <- confusionMatrix(predictions_svm, realvalues, mode="prec_recall", positive = "B")
 classLabelSVM
 classLabelSVM$byClass["Precision"]
 classLabelSVM$byClass["Recall"]
 classLabelSVM$byClass["F1"]
 
-precision_recallSVM = coords(svm.ROC, "all", ret = c("recall", "precision"), transpose = FALSE)
-precision_recallSVM = precision_recallSVM[complete.cases(precision_recallSVM), ]
-dat = data.frame(x = precision_recallSVM$recall, y = precision_recallSVM$precision)
+precision_recallSVM <- coords(svm.ROC, "all", ret = c("recall", "precision"), transpose = FALSE)
+precision_recallSVM <- precision_recallSVM[complete.cases(precision_recallSVM), ]
+dat <- data.frame(x = precision_recallSVM$recall, y = precision_recallSVM$precision)
 ggplot(dat, aes(x, y)) +
   geom_point() + 
   xlab("Recall") +
@@ -350,9 +353,9 @@ classLabelNNET$byClass["Precision"]
 classLabelNNET$byClass["Recall"]
 classLabelNNET$byClass["F1"]
 
-precision_recallNNET = coords(nnet.ROC, "all", ret = c("recall", "precision"), transpose = FALSE)
-precision_recallNNET = precision_recallNNET[complete.cases(precision_recallNNET), ]
-dat = data.frame(x = precision_recallNNET$recall, y = precision_recallNNET$precision)
+precision_recallNNET <- coords(nnet.ROC, "all", ret = c("recall", "precision"), transpose = FALSE)
+precision_recallNNET <- precision_recallNNET[complete.cases(precision_recallNNET), ]
+dat <- data.frame(x = precision_recallNNET$recall, y = precision_recallNNET$precision)
 ggplot(dat, aes(x, y)) +
   geom_point() + 
   xlab("Recall") +
